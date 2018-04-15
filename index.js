b5ef98ca995b565487d104799915d83797f6e2bf
github username <github email address>
2018-04-15 11:13:56 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+};
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list');
+    for (let i = 0; i < lis.length; i++) {
+      let children = lis[i].children;
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = Number(children[j].innerHTML) + n;
+    };
+  };
+};
+
+function deepestChild(){
+  const nodes = document.querySelectorAll('#grand-node div');
+  let deepestChild = nodes[nodes.length- 1];
+
+  return deepestChild;
+}
